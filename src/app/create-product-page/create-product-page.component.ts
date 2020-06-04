c636b9fca13cb03569d45c808b2d19402f20071d
javacf <flores.orihuela@gmail.com>
2020-06-04 08:39:20 -0500

    Table Material

--- src/app/create-product-page/create-product-page.component.ts
+++ src/app/create-product-page/create-product-page.component.ts
@@ -1,47 +1,55 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product';
+import { Type } from '../model/type';
 import { ProductService } from '../service/product.service';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-create-product-page',
+  selector: 'app-create-product',
   templateUrl: './create-product-page.component.html',
   styleUrls: ['./create-product-page.component.css']
 })
+
 export class CreateProductPageComponent implements OnInit {
 
-  displayedColumns = ['productName', 'productCode', 'prodRating', 'edit', 'delete'];
+  product: Product = new Product();
+  //types: Observable<Type>;
+  types : Type[];
+  mensaje : String;
 
-  Oberseproducts
-  
-  dataSource: MatTableDataSource<Product>;
+  constructor(private productService: ProductService, private router: Router) { }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort, {}) sort: MatSort;
-  
-  constructor(private productService: ProductService) {  
-   // this.productService.getProductList().subscribe(products => this.products = products);
+  ngOnInit(): void {
+    //cargando types
+    this.cargando();
   }
 
- 
- 
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim();
-    filterValue = filterValue.toLowerCase();
-    this.dataSource.filter = filterValue;
+  cargando(){
+    console.log("Cargando tipos")
+    this.productService.getTypes().subscribe(types => this.types = types);
+    console.log(this.types);
   }
-    
-    ngAfterViewInit() {
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+
+  save(){
+     console.log(this.product);
+     this.productService.createProduct(this.product).subscribe(
+       data => this.router.navigate(['/list'])
+     );
+
+  }
+
+  compararTipo(o1:Type, o2:Type) : boolean{
+    if (o1===undefined && o2===undefined){
+      return true;
     }
+     return o1 === null || o2 === null || o1 === undefined || o2 === undefined  ? false : o1.idType === o2.idType 
+  }
   
-     ngOnInit() {
-      //this.dataSource = this.products;
-      this.dataSource.paginator = this.paginator;
-  
-    }
+  changeType(evt) {
+      alert("Ho" + this.product.type.idType + ": " + evt);
+      this.mensaje = this.product.type.description;
+
+  }
+
 }
